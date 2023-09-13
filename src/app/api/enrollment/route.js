ce7a267016018893665b71943010f45acfed8823
TIT4nnnx <ekarach6627@gmail.com>
2023-09-13 23:39:50 +0700

    .

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -29,7 +29,7 @@ export const GET = async (request) => {
   }
   if (role === "ADMIN") {
     return NextResponse.json({
-      ok: true,
+      ok: false,
       enrollments: DB.enrollments,
     });
   }
